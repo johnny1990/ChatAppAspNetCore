b16f08525400a000359a15a22866d0e3b06f7dfe
Man Ioan <man_ioan@live.com>
2022-03-27 16:59:40 +0300

    Add project files.

---
+++ ChatAppAspNetCore/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ChatAppAspNetCore.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
